7d70d416b05a749e7e1ab563d2fb459ba15524fc
jirayuch01 <jirayu_ch01@hotmail.com>
2017-09-28 11:50:24 +0700

    update
    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
 var app = express();
 app.use(express.static(publicPath));
 app.listen(port, () => {
