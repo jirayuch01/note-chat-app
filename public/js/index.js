f8ca6128fa77d7fc4fb189937b40f4b90f294485
jirayuch01 <jirayu_ch01@hotmail.com>
2017-10-03 14:46:45 +0700

    update IO
    update IO

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server');
+    socket.emit('createMessage', {
+        from: 'Somchai',
+        text: 'Yup, that works for me.'
+    });
+});
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 const socketIO = require('socket.io');
 const http = require('http');
 
@@ -12,6 +12,14 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: 'Somsri',
+        text: 'See you then',
+        createdAt: 123123
+    });
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
@@ -19,6 +27,7 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
+
 
 // app.listen(port, () => {
 //     console.log(`Serve is up on port ${port}`);
