1f478674848282c1d1adce88688320d78f754367
jirayuch01 <jirayu_ch01@hotmail.com>
2017-10-05 07:32:45 +0700

    up
    up

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,21 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from: 'Somchai',
-        text: 'Yup, that works for me.'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'Somchai',
+    //     text: 'Yup, that works for me.'
+    // });
+    socket.emit('client', 'Hi');
+    socket.emit('client', 'How are you?');
+    socket.emit('client', 'Hi');
+    socket.emit('client', 'Hi');
 });
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
-socket.on('newMessage', function (message) {
-    console.log('newMessage', message);
+// socket.on('newMessage', function (message) {
+//     console.log('newMessage', message);
+// });
+socket.on('client', function (message) {
+    console.log('server:', message);
 });
--- server/server.js
+++ server/server.js
@@ -10,24 +10,25 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'Somsri',
-        text: 'See you then',
-        createdAt: 123123
-    });
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-    });
-    socket.on('disconnect', () => {
-        console.log('User was disconnected');
-    });
+    // socket.emit('newMessage', {
+    //     from: 'Somsri',
+    //     text: 'See you then',
+    //     createdAt: 123123
+    // });
+    // socket.on('createMessage', (message) => {
+    //     console.log('createMessage', message);
+    // });
+    // socket.on('disconnect', () => {
+    //     console.log('User was disconnected');
+    // });
 });
+
 server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 });
-
 
 // app.listen(port, () => {
 //     console.log(`Serve is up on port ${port}`);
